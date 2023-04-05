0160f15e3534ebabf3714ba7dbbe0e5c652e4dbc
Raul60rc <34682876+Raul60rc@users.noreply.github.com>
2023-04-05 17:05:15 +0200

    home component done & finished next is About component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,11 +11,11 @@ const Home = () => {
         <h2 className="text-4xl sm:text-7xl font-bold text-orange-500">I'm a Full Stack Developer & Data Scientist</h2>
         <p className="text-green-500 py-4 max-w-[700]">
           I'm a self taught programmer who has learnt first on FreeCodeCamp &
-          later in 2 different bootcamps. I have a big passion for coding & the
-          financial markets in future I aspire to get into Quantative Trading.
+          later in 2 different bootcamps. I have a big passion for coding,the
+          financial markets & in future I aspire to get into Quantative Trading, Machine Learning & Ai.
         </p>
         <div>
-            <button className="text-white group border-2 px-6 py-3 my-2 items-center hover:bg-pink-600 hover:border-pink-600">
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
                 View More
             <span className="group-hover:rotate-90 duration-300">
             <HiArrowNarrowRight className="ml-3"/>
